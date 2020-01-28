55b7bdcd1afc0f6e3003b3cd64043ef87aba2020
Kathya Severino <kathya.severino@gmail.com>
2020-01-28 18:29:38 -0500

    add server variable

--- app.py
+++ app.py
@@ -159,6 +159,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div(children=[
     html.H1(children=''),
     dcc.Graph(
